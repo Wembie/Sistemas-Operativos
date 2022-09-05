b72744fd8a79a2991ea988825f89f5546ad110ea
Juan Acosta <73087520+Wembie@users.noreply.github.com>
2022-09-04 20:51:58 -0500

    Add files via upload
    Faltan cositas y arreglas errocitos pequeñitos que son minoria

--- Proyecto 1/Proyecto.c
+++ Proyecto 1/Proyecto.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
 #define MAX_LINE 80 /* The maximum length command */
 
+void ejecutarPipe( char ** argsTemp, int pipe, int * run ){
+    if( pipe == 0 ){ //No tiene Pipe
+        argsTemp[ 2 ] = NULL;
+        int pid = fork();
+        if( pid < 0 ){
+            printf( "ERROR" );
+            *run = 0;
+        }
+        else if( pid == 0 ){ //Hijo
+            execvp( argsTemp[ 0 ], argsTemp );
+            exit( 0 );
+        }
+        else if( pid == 1 ){ //Padre
+            wait( NULL );
+        }
+    }
+    else{ //Tiene Pipe
+        //To do
+    }
+} 
+
+int dividirLinea( char * args, char ** argsTemp ){
+    int i = 0, indicePipe = 0;
+    char * ptr = strtok( args, " " );
+    while( ptr != NULL ){
+        argsTemp[ i++ ] = ptr ;
+        if( strcmp( ptr, "|" ) == 0 ){
+            indicePipe = i - 1;
+        }
+        ptr = strtok( NULL, " " );
+    }
+    if( args[ strlen( args ) - 1 ] == '\n' ){
+        args[ strlen( args ) - 1 ] = '\0';
+    }
+    //Falta quitar el \n en el argstemp
+    return indicePipe;
+}
+
 int main( void ){
     char args[ MAX_LINE ]; /* command line arguments */
+    char historia[  ] = "No commands in history.";
     int run = 1; /* flag to determine when to exit program */
+
     while( run ){
-        int pid, i;
-        char del[ MAX_LINE ] = " ";
-        char argsTemp[ 20 ][ MAX_LINE ];
+        int pid, tienePipe;
+        char * argsTemp[ MAX_LINE ];
         char * ptr;
         printf( "osh > " );
-        fflush( stdin );
         fflush( stdout ); //Vaciar Buffer
         /**
         * After reading user input, the steps are:
@@ -22,32 +61,14 @@ int main( void ){
         * (2) the child process will invoke execvp()
         * (3) parent will invoke wait() unless command included &
         */
-        pid = fork();
+        //pid = fork();
         fgets( args, MAX_LINE, stdin );
-        ptr = strtok( args, del );
-        i = 0;
-        while( ptr != NULL ){
-            strcpy( argsTemp[ i++ ], ptr );
-            ptr = strtok( NULL, del );
-        }
-        if( args[ strlen(args) - 1 ] == '\n' ){
-            args[ strlen(args) - 1 ] = '\0';
-        }
+        tienePipe = dividirLinea( args, argsTemp );
         if( strcmp( args, "exit" ) == 0 ){
             run = 0;
         }
         else{
-            if( pid < 0 ){
-                printf( "ERROR" );
-                run = 0;
-            }
-            else if( pid == 0 ){ //Hijo
-                //execvp( argsTemp[0],  )
-            }
-            else if( pid == 1 ){ //Padre
-                //wait( NULL );
-                printf("Child complete\n");
-            }
+            ejecutarPipe( argsTemp, tienePipe, &run );
         }
         printf( "\n" );
     }
